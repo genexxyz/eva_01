57ff7e1934dae1181419c6b2858e2eb6172e236c
erika <erikamaepolka0209@gmail.com>
2024-03-18 09:16:21 +0800

    update model

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -3,12 +3,9 @@
 class Home extends Controller{
     public function index(){
     $this->view('home');
-<<<<<<< HEAD
-=======
         
         $model = new Model();
         $arr['firstname'] = "dio";
->>>>>>> a7e8e9df4563908e54ae1293d83b52c14a1e4749
 
         $data = $model->where($arr);
         show($data);
